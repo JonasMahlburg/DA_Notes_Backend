3fc472a2f936b3aabc7adb8644d7a021af14ede8
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-26 08:29:33 +0200

    fixing Problems with Note model and Endpoints

--- notes_app/models.py
+++ notes_app/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    note = models.TextField(max_length=500)
+    content = models.TextField(max_length=500)
     title = models.CharField(max_length=50)
     date = models.DateTimeField((), auto_now=False, auto_now_add=True)
+    marked = models.BooleanField(default=False)
+    trash =  models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title}, {self.note}, ({self.date})"
